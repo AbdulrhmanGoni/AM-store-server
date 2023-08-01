3afd6a834cbc889a9341bcb9dc7b974f4828c3d8
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2023-08-01 18:28:40 +0300

    modified comments getter function to add slicing logic

--- user_actions/comments_getComments.js
+++ user_actions/comments_getComments.js
@@ -5,16 +5,22 @@ import UserModel from "../models/Users.js";
 const comments_getComments = async (req, res) => {
     try {
         const { productId } = req.params;
-        const comments = await CommentsModule.find({ productId }, { replies: false });
-        for (let i = 0; i < comments.length; i++) {
+        const { sliceNumber, sliceSize } = req.query;
+        const range = +sliceNumber * +sliceSize;
+        const comments = await CommentsModule.find({ productId }, { replies: false }, { skip: range, limit: +sliceSize + 1 });
+        const thereIsMore = !!comments[sliceSize];
+        const loopLength = +sliceSize + 1 == comments.length ? +sliceSize : comments.length;
+        for (let i = 0; i < loopLength; i++) {
             const comment = comments[i];
             const { replies } = await CommentsModule.findById(comment._id, { replies: { _id: true } });
             const userData = await UserModel.findById(comment.commenterId, userDataTypes.comment);
             comment.commenterData = userData;
             comment.replies = replies;
         }
-        res.status(200).json(comments)
-    } catch { res.status(400).json([]) }
+        res.status(200).json({ comments: comments.slice(0, thereIsMore ? -1 : undefined), thereIsMore });
+    } catch (err) {
+        res.status(400).json({})
+    }
 }
 
 export default comments_getComments;
