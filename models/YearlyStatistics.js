b8fdf3705a336859b4891a49b83cf10f52c3cc65
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2023-09-16 14:28:47 +0300

    Rename property in `YearlyStatisticsSchema` Rename `date` property to `year` in `YearlyStatisticsSchema`

--- models/YearlyStatistics.js
+++ models/YearlyStatistics.js
@@ -1,7 +1,7 @@
 import { model, Schema } from "mongoose";
 
 const YearlyStatisticsSchema = new Schema({
-    date: { type: String },
+    year: { type: String },
     statisticsType: { type: String },
     categories: { type: Object },
     monthes: { type: Array },
