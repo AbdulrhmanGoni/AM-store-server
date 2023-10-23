5c4a3ecf5864598780947c3b87d4e91c1134a06b
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2023-10-23 15:05:54 +0300

    Add an option to a propertyin `UserSchema` Set the default value option of `emailIsVerified` property to 'false'.

--- models/Users.js
+++ models/Users.js
@@ -45,7 +45,10 @@ const UserSchema = new Schema({
     },
     userFavorites: [Types.ObjectId],
     userOrders: [Types.ObjectId],
-    emailIsVerified: Boolean
+    emailIsVerified: {
+        type: Boolean,
+        default: false
+    }
 },
     { timestamps: true }
 );
