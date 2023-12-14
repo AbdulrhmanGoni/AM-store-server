b823488f2f1d7191f7c8df18693c3e52e260ea45
Abdulrhman <abdulrhmangoni@gmail.com>
2023-12-14 14:23:28 +0300

    Create `removeDiscountFromProducts` function And fix an error in `addDiscountToProducts` function.

--- src/controllers/admin-controllers/removeDiscountFromProducts.js
+++ src/controllers/admin-controllers/removeDiscountFromProducts.js
@@ -1,9 +1,9 @@
 import ProductsModel from "../../models/Products.js";
 
-export default async function addDiscountToProducts(productsIds, discount) {
+export default async function removeDiscountFromProducts(productsIds) {
     try {
         const filter = { _id: { $in: productsIds } }
-        const updateQuery = { $set: { discount } }
+        const updateQuery = { $set: { discount: 0 } }
         const result = await ProductsModel.updateMany(filter, updateQuery);
         return !!result.modifiedCount;
     } catch (error) {
--- src/routes/admin_routes/products_removeDiscount_post.js
+++ src/routes/admin_routes/products_removeDiscount_post.js
@@ -1,9 +1,9 @@
 import AdminController from "../../controllers/admin-controllers/AdminController.js";
 
-export default async function products_addDiscount_post(req, res) {
+export default async function products_removeDiscount_post(req, res) {
     try {
-        const { productsIds, discount } = req.body;
-        const response = await AdminController.addDiscountToProducts(productsIds, discount);
+        const { productsIds } = req.body;
+        const response = await AdminController.removeDiscountFromProducts(productsIds);
         res.status(response ? 200 : 400).json(response);
     } catch (error) {
         res.status(400).json(null);
