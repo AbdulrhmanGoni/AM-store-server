0de2c7700118eff38785061507db101a4ee392d1
Abdulrhman <abdulrhmangoni@gmail.com>
2023-12-30 00:29:54 +0300

    Refactor and fix some errors in `logInUserWithGoogle`

--- src/controllers/system-controller/logInUserWithGoogle.js
+++ src/controllers/system-controller/logInUserWithGoogle.js
@@ -1,15 +1,15 @@
 import jwt from "jsonwebtoken";
 import UsersModel from "../../models/Users.js";
 import googleAccountGetter from "../../utilities/googleAccountGetter.js";
+import { errorResponse } from "./logInAdmin.js";
 
 export default async function logInUserWithGoogle(googleUserAccessToken) {
     try {
-        const notFoundResponse = { message: "This email did not signed up with us before, Go to Sign up page" };
-        const unexpectedError = { message: "There is unexpected error happened" };
         const response = await googleAccountGetter(googleUserAccessToken);
-        if (response) {
+        const { googleResponse, ok } = response
+        if (ok) {
             const projection = { userPassword: 1, signingMethod: 1 }
-            const userData = await UsersModel.findOne({ userEmail: response.email }, projection);
+            const userData = await UsersModel.findOne({ userEmail: googleResponse.email }, projection);
             if (userData) {
                 if (userData.signingMethod === "Google auth") {
                     const userId = userData._id;
@@ -18,16 +18,15 @@ export default async function logInUserWithGoogle(googleUserAccessToken) {
                         process.env.JWT_SECRET_KEY,
                         { expiresIn: "30d" }
                     )
-                    return { status: true, userId, accessToken: token };
+                    return { status: 200, response: { ok: true, userId, accessToken: token } };
                 }
-                else return { message: "This email signed up with another sign up method" }
+                else return errorResponse("This email signed up with another sign up method")
             }
-            else return notFoundResponse
+            else return errorResponse("This email did not signed up with us before, Go to Sign up page")
         }
-        else if (response === null) return notFoundResponse
-        else return unexpectedError
+        else return googleResponse
     } catch (error) {
         console.log(error);
-        return unexpectedError
+        return errorResponse("There is unexpected error happened")
     }
 }
--- src/routes/root_routes/logIn_user_withGoogle_post.js
+++ src/routes/root_routes/logIn_user_withGoogle_post.js
@@ -4,10 +4,9 @@ export default async function logIn_user_withGoogle_post(req, res) {
     try {
         const { googleUserAccessToken } = req.body;
         const response = await SystemController.logInUserWithGoogle(googleUserAccessToken);
-        if (response.status) res.status(200).json(response);
-        else res.status(400).json(response);
+        res.status(response.status).json(response.response);
     } catch (error) {
         console.log(error);
-        res.status(400).json({ message: "There is unexpected error happened" });
+        res.status(500).json({ message: "There is unexpected server error happened" });
     }
 }
