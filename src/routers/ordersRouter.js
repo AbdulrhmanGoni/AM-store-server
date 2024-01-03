bbe21bdf178d1d9ef52fc1a2bfa3af130ec82da2
Abdulrhman <abdulrhmangoni@gmail.com>
2024-01-03 20:08:57 +0300

    Create "/latest-orders" route in `ordersRouter` And create `orders_latestOrders_get` handler and make it uses `getLatestOrders` controller function from `OrdersControllers` to provide the service

--- src/routers/ordersRouter.js
+++ src/routers/ordersRouter.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 import orders_pagination_get from "../routes/orders_routes/orders_pagination_get.js";
 import orders_getOrderById_get from "../routes/orders_routes/orders_getOrderById_get.js";
 import orders_cancelOrder_delete from "../routes/orders_routes/orders_cancelOrder_delete.js";
+import orders_latestOrders_get from "../routes/orders_routes/orders_latestOrders_get.js";
+import adnminAuth from "../auth/adminAuth.js";
 
 const router = Router();
+
+router.route("/latest-orders").get([adnminAuth, orders_latestOrders_get]);
 
 router.route("/pagenation").get(orders_pagination_get);
 
---
+++ src/routes/orders_routes/orders_latestOrders_get.js
@@ -0,0 +1,10 @@
+import OrdersControllers from "../../controllers/orders-controllers/OrdersControllers.js";
+
+export default async function orders_latestOrders_get(req, res) {
+    try {
+        const result = await OrdersControllers.getLatestOrders(req.limit);
+        res.status(result ? 200 : 400).json(result);
+    } catch (error) {
+        res.status(500).json();
+    }
+}
