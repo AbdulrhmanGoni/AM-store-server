a9301590d02540e90252ea9814b50bf53bedfe46
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-01-29 01:49:13 +0300

    Revert "Revert "k""
    This reverts commit c99648ea91ab5cae1f100b4e703f6b0345b82c4d.

--- __tests__/integration_testing/products_routes_tests/products_userSearch_get.test.js
+++ __tests__/integration_testing/products_routes_tests/products_userSearch_get.test.js
@@ -24,7 +24,7 @@ describe("Test 'products_userSearch_get' route handler", () => {
     })
 
     it("Should returns an array of figures products", async () => {
-        const response = await anyRequest(`${routePath}?category=figures`)
+        const response = await anyRequest(`${routePath}?category=figures`, "get")
         expect(response.statusCode).toBe(200)
         response.body.forEach(product => {
             expect(product.category).toBe("figures")
@@ -32,7 +32,7 @@ describe("Test 'products_userSearch_get' route handler", () => {
     })
 
     it("Should returns an array of Attack On Titan series products", async () => {
-        const response = await anyRequest(`${routePath}?series=Attack On Titan`)
+        const response = await anyRequest(`${routePath}?series=Attack On Titan`, "get")
         expect(response.statusCode).toBe(200)
         response.body.forEach(product => {
             expect(product.series).toBe("Attack On Titan")
@@ -40,7 +40,7 @@ describe("Test 'products_userSearch_get' route handler", () => {
     })
 
     it("Should returns an array of figurs products with titles match 'naruto'", async () => {
-        const response = await anyRequest(`${routePath}?title=naruto`)
+        const response = await anyRequest(`${routePath}?title=naruto`, "get")
         expect(response.statusCode).toBe(200)
         response.body.forEach(product => {
             expect(product.title).toMatch(/naruto/i)
@@ -48,13 +48,13 @@ describe("Test 'products_userSearch_get' route handler", () => {
     })
 
     it("Should returns an empty array with status code 404", async () => {
-        const response = await anyRequest(`${routePath}?title=luffy&category=soccer`)
+        const response = await anyRequest(`${routePath}?title=luffy&category=soccer`, "get")
         expect(response.statusCode).toBe(404)
         expect(response.body.length).toBe(0)
     })
 
     it("Should returns an empty array with status code 404", async () => {
-        const response = await anyRequest(`${routePath}?title=ftnjgdjtmd`)
+        const response = await anyRequest(`${routePath}?title=ftnjgdjtmd`, "get")
         expect(response.statusCode).toBe(404)
         expect(response.body.length).toBe(0)
     })
