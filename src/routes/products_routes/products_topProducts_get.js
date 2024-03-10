57f859dbb76d5b44be8b011ba2868a058de64c28
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-03-10 13:45:52 +0300

    Cache top products list & add caching option to search function
    - Use `cacheOrQuery` function in `products_topProducts_get` route handler
    to cache top products list for one hour and then revalidate the cache.
    
    - Add caching option to `products_userSearch_get` route handler through
    `cacheFor` query parameter, the query parameter should consist of a strings
    of two parts separated by `-`, the first part represents the type of the time
    (E.g, hours, minutes, weeks, ect...), and the second part represents the
    number of the given time in the first part.

--- src/routes/products_routes/products_topProducts_get.js
+++ src/routes/products_routes/products_topProducts_get.js
@@ -1,10 +1,14 @@
+import cacheOrQuery from "../../cache/cacheOrQuery.js";
 import ProductsController from "../../controllers/products-controllers/ProductsController.js";
 import asyncRouteHandler from "../../utilities/asyncRouteHandler.js";
+
+const CACHE_EXPIRATION_DATE = 60 * 60 // seconds * minutes
 
 export default asyncRouteHandler(
     async function products_topProducts_get(req, res) {
         const { limit } = req.query;
-        const result = await ProductsController.getTopProducts(limit);
-        res.status(result ? 200 : 400).json(result)
+        const key = `top-products-${limit}`
+        const response = await cacheOrQuery(key, ProductsController.getTopProducts(limit), CACHE_EXPIRATION_DATE)
+        res.status(response ? 200 : 400).json(response)
     }
 )
--- src/routes/products_routes/products_userSearch_get.js
+++ src/routes/products_routes/products_userSearch_get.js
@@ -1,16 +1,24 @@
 import { productDataTypes } from "../../CONSTANT/projections.js";
+import cacheOrQuery from "../../cache/cacheOrQuery.js";
 import ProductsController from "../../controllers/products-controllers/ProductsController.js";
 import asyncRouteHandler from "../../utilities/asyncRouteHandler.js";
+import genTime from "../../utilities/genTime.js";
 
 
 export default asyncRouteHandler(
     async function products_userSearch_get(req, res) {
-        const result = await ProductsController.search({
+        const { query: { cacheFor, type }, url } = req
+        const searchOptions = {
             queries: req.query,
-            projection: productDataTypes[req.query.type ?? "basic"]
-        });
-        if (result) {
-            res.status(200).json(result);
-        } else res.status(400).json();
+            projection: productDataTypes[type ?? "basic"]
+        }
+        let response;
+        if (cacheFor) {
+            const expirationDate = genTime(...cacheFor?.split("-")?.reverse())
+            response = await cacheOrQuery(url, ProductsController.search(searchOptions), expirationDate / 1000)
+        } else {
+            response = await ProductsController.search(searchOptions);
+        }
+        res.status(response ? 200 : 400).json(response);
     }
 )
