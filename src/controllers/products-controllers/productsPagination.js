3e02bd70c5cbd5c4841b069ba826b00c757ee6d2
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-04-10 00:19:23 +0300

    feat: Add categories filter option to `productsPagination` function
    Make `productsPagination` function looks at `categories` query for filtering the
    products based of the categories that provided through `categories` query if exist.

--- src/controllers/products-controllers/productsPagination.js
+++ src/controllers/products-controllers/productsPagination.js
@@ -4,14 +4,29 @@ import ProductsModel from "../../models/Products.js";
 
 
 export default async function productsPagination(query) {
-    const { page = 1, pageSize = 10, returnType, type } = query;
+    const { page = 1, pageSize = 10, returnType, type, categories } = query;
     const skip = (page - 1) * pageSize;
     const projection = returnType ? createProjection(returnType) : productDataTypes[type ?? "basic"];
     try {
-        const products = await ProductsModel.find({}, projection, { limit: +pageSize + 1, skip });
+        const categoriesFilter = createCategoriesFilter(categories);
+        const products = await ProductsModel.find(
+            categoriesFilter ? { $or: categoriesFilter } : {},
+            projection,
+            { limit: +pageSize + 1, skip }
+        );
         const thereIsNextPage = !!products[pageSize];
-        return products.slice(0, thereIsNextPage ? -1 : undefined)
+        return {
+            products: products.slice(0, thereIsNextPage ? -1 : undefined),
+            thereIsNextPage
+        }
     } catch {
-        return []
+        return null
     }
+}
+
+function createCategoriesFilter(categories) {
+    if (categories) {
+        return categories.split(",").map((category) => ({ category }))
+    }
+    return null
 }
