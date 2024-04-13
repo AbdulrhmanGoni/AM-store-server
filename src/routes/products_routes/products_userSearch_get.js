4f425c07ba15d053988972d68ee59dfe77603b22
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-04-14 01:45:57 +0300

    refactor: change the way of setting expiration date for search result cache
    Set a fixed expiration date for products search result cache instead of using `cacheFor`
    query parameter to specify the expiration date, That is in order not to give the client
    side the ability to set the expiration date for products search result cache.
    
    Use `useCache` query parameter to determine if the client wants to get the cached
    search result if exist or wants to get a fresh data from the database directly.

--- src/routes/products_routes/products_userSearch_get.js
+++ src/routes/products_routes/products_userSearch_get.js
@@ -4,17 +4,18 @@ import ProductsController from "../../controllers/products-controllers/ProductsC
 import asyncRouteHandler from "../../utilities/asyncRouteHandler.js";
 import genTime from "../../utilities/genTime.js";
 
+const CACHE_EX_DATE_IN_MINUTES = 20
 
 export default asyncRouteHandler(
     async function products_userSearch_get(req, res) {
-        const { query: { cacheFor, type }, url } = req
+        const { query: { useCache, type }, url } = req
         const searchOptions = {
             queries: req.query,
             projection: productDataTypes[type ?? "basic"]
         }
         let response;
-        if (cacheFor) {
-            const expirationDate = genTime(...cacheFor?.split("-")?.reverse())
+        if (useCache === "true") {
+            const expirationDate = genTime("minutes", CACHE_EX_DATE_IN_MINUTES)
             response = await cacheOrQuery(url, ProductsController.search(searchOptions), expirationDate / 1000)
         } else {
             response = await ProductsController.search(searchOptions);
