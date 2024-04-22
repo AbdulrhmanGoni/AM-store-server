3c75725b854b23b235e2e862cfbb8487a126312e
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-04-22 14:33:26 +0300

    refactor: delete a deprecated controller / route handler
    Delete `getProductsCategoriesList` controller function and `productsCategorieslist_get`
    route handler.
    
    Delete `productsCategoriesList.json` file that was stores products categories list
    for `getProductsCategoriesList` controller function.
    
    Use `cacheOrQuery` function to get products categories list in `getYearStatisticsDocument`
    controller function instead of using `getProductsCategoriesList`.

--- src/controllers/settings-controllers/SettingsController.js
+++ src/controllers/settings-controllers/SettingsController.js
@@ -1,6 +1,5 @@
 import getSettings from "./getSettings.js"
 import updateSetting from "./updateSetting.js"
-import getProductsCategoriesList from "./getProductsCategoriesList.js"
 import getStoreVariables from "./getStoreVariables.js"
 import getDiscountCobones from "./getDiscountCobones.js"
 import addDiscountCobone from "./addDiscountCobone.js"
@@ -13,7 +12,6 @@ class SettingsController {
 export default Object.assign(SettingsController.prototype, {
     getSettings,
     updateSetting,
-    getProductsCategoriesList,
     getStoreVariables,
     getDiscountCobones,
     addDiscountCobone,
--- src/controllers/statistics-controllers/getYearStatisticsDocument.js
+++ src/controllers/statistics-controllers/getYearStatisticsDocument.js
@@ -1,13 +1,15 @@
+import cacheOrQuery from "../../cache/cacheOrQuery.js";
 import { MONTHES } from "../../CONSTANT/MONTHES.js"
 import YearlyStatisticsModel from "../../models/YearlyStatistics.js";
-import getProductsCategoriesList from "../settings-controllers/getProductsCategoriesList.js";
+import SettingsController from "../../controllers/settings-controllers/SettingsController.js";
 
 export default async function getYearStatisticsDocument(year = new Date().getFullYear(), { createDirectly, save } = {}) {
 
     const yearStatistics = !createDirectly && await YearlyStatisticsModel.findOne({ year });
     if (yearStatistics) return yearStatistics;
     else {
-        const productsCategories = await getProductsCategoriesList();
+        const storeVariables = await cacheOrQuery("store-variables", SettingsController.getStoreVariables());
+        const productsCategories = storeVariables.productsCategories
         const newYearStatisticsDocument = new YearlyStatisticsModel({
             year,
             categories: productsCategories.map((category) => {
