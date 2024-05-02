bad783bd175216ddb1eb4f0090daf87d3a4a3109
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-02 15:24:16 +0300

    test: refactor tests scripts
    Rename `/jest-runner-script.js` script file to `run-jest-tests.js`
    
    Remove `testing-db` and `docker-test` scripts from `package.json` file,
    And make `test` script uses `mongodb-replSet` script for running the
    databases before starting the tests.
    
    Remove keywords section in `package.json` because it doesn't make
    sense in this case of the server.
    
    Rename environment variables file that is used in testing environment
    in `continuous-deployment.yml` workflow file.

--- scripts/run-jest-tests.js
+++ scripts/run-jest-tests.js
@@ -3,7 +3,7 @@ import { execSync } from 'child_process';
 const runningTarget = process.argv[2] || "";
 
 const command = `\
-    env-cmd -f .env.secret env-cmd -f .env.jest-testing node \
+    env-cmd -f .env.testing node \
     --experimental-vm-modules node_modules/jest/bin/jest.js \
     --runInBand \
     --detectOpenHandles \
@@ -12,7 +12,7 @@ const command = `\
 `;
 
 try {
-    execSync(command, { stdio: 'inherit', shell: 'bash' });
+    execSync(command, { stdio: 'inherit' });
 } catch (error) {
     console.error('Error executing Jest tests:');
     process.exit(1);
