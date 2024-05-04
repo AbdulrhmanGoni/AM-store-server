ff4c38be3f6afdf972af217ec3fde8bb7199c195
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-04 13:59:23 +0300

    chore: set a default `JWT` key

--- src/utilities/jwtUtilities.js
+++ src/utilities/jwtUtilities.js
@@ -1,9 +1,11 @@
 import jwt from "jsonwebtoken";
 
+const DEFAULT_JWT_KEY = "default-jwt-key"
+
 export function generateJWT(payload, expiresIn = "30d") {
-    return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn });
+    return jwt.sign(payload, process.env.JWT_SECRET_KEY || DEFAULT_JWT_KEY, { expiresIn });
 }
 
 export function verifyJWT(token) {
-    return jwt.verify(token, process.env.JWT_SECRET_KEY)
+    return jwt.verify(token, process.env.JWT_SECRET_KEY || DEFAULT_JWT_KEY)
 }
